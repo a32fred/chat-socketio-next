b051f0f6320ab96d9b1b0dea8ef7c052f8dc0d69
a32fred <a.32.freeed@gmail.com>
2023-04-16 14:18:35 -0300

    chat viewer

---
+++ src/pages/chat.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+
+// Endereço do servidor Socket.IO
+const socket = io("http://localhost:3322/", { transports: ['websocket'] });
+
+export default function Chat() {
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    socket.on('chat message', (msg) => {
+      setMessages([...messages, msg]);
+    });
+
+    return () => {
+      socket.off('chat message');
+    };
+  }, [messages]);
+
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    socket.emit("chat message", input);
+    setInput("");
+
+  };
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="flex-1 overflow-y-scroll bg-gray-950 text-white">
+        {messages.map((message, index) => (
+          <div key={index} className="p-2">
+            <div className="flex items-end">
+              <div className="bg-gray-800 rounded-lg p-2 max-w-xs">
+                <p className="text-sm break-words">{message}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <form onSubmit={handleSubmit} className="flex p-2 bg-slate-600 ">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => { setInput(e.target.value) }}
+          placeholder="Digite uma mensagem"
+          className="flex-1 bg-gray-800 rounded-lg p-2 text-white"
+        />
+
+        <button type="submit" className="ml-2 bg-indigo-500 rounded-lg p-2 text-white">
+          Enviar
+        </button>
+      </form>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,9 @@
-import { useState, useEffect } from "react";
-import io from "socket.io-client";
-
-// Endereço do servidor Socket.IO
-const socket = io("https://socketio.a32fred.repl.co", {transports:['websocket']});
+import Chat from "./chat"
 
 export default function Home() {
-  const [messages, setMessages] = useState([]);
-  const [input, setInput] = useState("");
-
-  useEffect(() => {
-    socket.on('chat message', (msg) => {
-      setMessages([...messages, msg]);
-    });
-
-    return () => {
-      socket.off('chat message');
-    };
-  }, [messages]);
-
-
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    
-    socket.emit("chat message", input);
-    setInput("");
-
-  };
-
   return (
-    <div className="bg-body flex flex-col h-screen">
-      <ul className="bg-gray-800 space-y-4 rounded-lg p-4 overflow-y-scroll flex-1">
-        {messages.map((msg, index) => (
-          <li className="text-slate-200" a key={index}>{msg}</li>
-        ))}
-      </ul>
-      <form onSubmit={handleSubmit}>
-        <input
-          className="bg-slate-500"
-          placeholder="digite aq a msg"
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button className="padding: 10px text-center" type="submit">
-          Enviar
-        </button>
-      </form>
+    <div className="">
+      <Chat />
     </div>
-  );
+  )
 }
