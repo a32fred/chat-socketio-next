6eb8279a01eb8278258c525b17bd7637b8d56655
Fred <85044631+a32fred@users.noreply.github.com>
2023-10-16 22:36:48 -0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,3 @@
-import { AuthProvider } from '../context/authContext'
 import { Analytics } from '@vercel/analytics/react';
 import '@/styles/globals.css'
 import 'socket.io-client/dist/socket.io.js'
@@ -8,9 +7,7 @@ import 'socket.io-client/dist/socket.io.js'
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
+      <Component {...pageProps} />
       <Analytics />
     </>
 
