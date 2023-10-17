ab9b81331b6f3c04700655565d22490a2b39a7bc
Fred <85044631+a32fred@users.noreply.github.com>
2023-10-17 21:23:02 +0000

    00557774

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -21,7 +21,7 @@ const Chat = () => {
         return;
       }
       setUser(savedUsername);
-      const newSocket = io("https://socketio.a32fred.repl.co", {transports: ["websocket"], query: { token: savedToken, userID: savedUsername } });
+      const newSocket = io("https://socketio.a32fred.repl.co", {transports: ["websocket"], query: { token: savedToken, userId: savedUsername } });
       setSocket(newSocket);
 
       if ("Notification" in window) {
